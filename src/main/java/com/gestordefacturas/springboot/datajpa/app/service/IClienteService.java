126eed681fc70f9de0fd5b0b5df1983045f0a2b4
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2025-07-16 19:16:08 +0200

    add observaciones detalles

--- src/main/java/com/gestordefacturas/springboot/datajpa/app/service/IClienteService.java
+++ src/main/java/com/gestordefacturas/springboot/datajpa/app/service/IClienteService.java
@@ -24,7 +24,9 @@ public interface IClienteService {
 
     void saveFactura(Factura factura);
 
-    public Producto findProductoById(Long id);
+    Producto findProductoById(Long id);
 
     Factura findFacturaById(Long id);
+
+    void deleteFactura(Long id);
 }
